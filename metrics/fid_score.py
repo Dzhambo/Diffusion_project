04250127e949cf80e3f3b2d63f6c966c755eb942
Dzhambo <dzhambulat.mollaev2012@yandex.ru>
2023-05-06 22:35:45 +0300

    Add fid_score

--- metrics/fid_score.py
+++ metrics/fid_score.py
@@ -1,19 +1,91 @@
 import torch
+from torch import nn
+from torchvision.models import inception_v3
+import cv2
+import multiprocessing
 import numpy as np
-from torchvision.models.inception import inception_v3
+from scipy import linalg
 
 
-def fid_score(images1, images2, device):
+def to_cuda(elements):
+    if torch.cuda.is_available():
+        return elements.cuda()
+    return elements
 
-    inception_model = inception_v3(pretrained=True, transform_input=False).to(device)
-    inception_model.eval()
 
-    embs1 = inception_model(images1)
-    embs2 = inception_model(images2)
+class PartialInceptionNetwork(nn.Module):
+    def __init__(self, transform_input=True):
+        super().__init__()
+        self.inception_network = inception_v3(pretrained=True)
+        self.inception_network.Mixed_7c.register_forward_hook(self.output_hook)
+        self.transform_input = transform_input
 
-    mu1, sigma1 = embs1.mean(dim=0), torch.cov(embs1)
-    mu2, sigma2 = embs2.mean(dim=0), torch.cov(embs2)
+    def output_hook(self, module, input, output):
+        self.mixed_7c_output = output
 
-    fid_score = torch.nn.MSELoss()(mu1, mu2) + torch.trace(sigma1 + sigma2 - 2 * torch.sqrt(sigma1 * sigma2))
+    def forward(self, x):
+        x = x * 2 -1
 
-    return fid_score
+        self.inception_network(x)
+
+        activations = self.mixed_7c_output
+        activations = torch.nn.functional.adaptive_avg_pool2d(activations, (1,1))
+        activations = activations.view(x.shape[0], 2048)
+        return activations
+
+
+def get_activations(images, batch_size):
+    num_images = images.shape[0]
+    inception_network = PartialInceptionNetwork()
+    inception_network = to_cuda(inception_network)
+    inception_network.eval()
+    n_batches = int(np.ceil(num_images  / batch_size))
+    inception_activations = np.zeros((num_images, 2048), dtype=np.float32)
+    for batch_idx in range(n_batches):
+        start_idx = batch_size * batch_idx
+        end_idx = batch_size * (batch_idx + 1)
+
+        ims = images[start_idx:end_idx]
+        ims = to_cuda(ims)
+        activations = inception_network(ims)
+        activations = activations.detach().cpu().numpy()
+        inception_activations[start_idx:end_idx, :] = activations
+    return inception_activations
+
+def calculate_activation_statistics(images, batch_size):
+    act = get_activations(images, batch_size)
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    diff = mu1 - mu2
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = "fid calculation produces singular product; adding %s to diagonal of cov estimates" % eps
+        warnings.warn(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError("Imaginary component {}".format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
+
+def fid_score(images1, images2,  batch_size=100):
+    up = torch.nn.Upsample(size=(299, 299), mode='bilinear')
+    mu1, sigma1 = calculate_activation_statistics(up(images1), batch_size)
+    mu2, sigma2 = calculate_activation_statistics(up(images2), batch_size)
+    fid = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+    return fid
